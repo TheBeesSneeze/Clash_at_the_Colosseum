66a6833bcf7b09f65e4d530baf14e4934f351805
ClareG2004 <cgrady@mail.bradley.edu>
2024-08-30 19:17:05 -0500

    First BEnemy push

---
+++ Assets/Scripts/Enemy/EnemyBasics.cs
@@ -0,0 +1,20 @@
+/*******************************************************************************
+* File Name :         EnemyBasics
+* Author(s) :         Clare Grady
+* Creation Date :     8/28/2024
+*
+* Brief Description : 
+* Basic Variables ALL enemies have
+* Die Function
+ *****************************************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBasics : MonoBehaviour
+{
+    public float EnemyHealth = 10f;
+    public float EnemySpeed = 1.0f;
+    public float EnemyDamage = 1.0f; 
+
+}
