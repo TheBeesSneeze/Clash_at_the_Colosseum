9f39d5e4e016f5d64ab6b2a113769bd63336473c
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2024-09-03 22:03:25 -0500

    Main menu settings page working

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -9,14 +9,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour
 {
+
+    public GameObject buttonContianer; 
     public GameObject playButton;
-    public int playSceneNumber;
     public GameObject settingsButton;
     public GameObject statsButton;
     public GameObject helpButton;
     public GameObject quitButton;
+
+    public GameObject settingsMenu;
+    public GameObject backButton;
+    public GameObject volumeSlider;
+    public GameObject sensitivitySlider;
+    public GameObject inputSwitch;
+    public GameObject inputSwitchText;
+
+    public GameObject wantToQuitButton;
+
+    public int playSceneNumber;
+    private void Start(){
+        InputEvents.Instance.PauseStarted.AddListener(escPressed);
+    }
+    public void escPressed()
+    {
+        if (settingsMenu.activeSelf){
+            settingsMenu.SetActive(false);
+            buttonContianer.SetActive(true);
+        } else if (wantToQuitButton.activeSelf){
+            buttonContianer.SetActive(true);
+            wantToQuitButton.SetActive(false);
+        } else {
+            buttonContianer.SetActive(false);
+            wantToQuitButton.SetActive(true);
+        }
+    }
     public void playClicked()
     {
         print("Loading Game...");
@@ -25,7 +54,8 @@ public class MainMenuScript : MonoBehaviour
     }
     public void settingsClicked()
     {
-        print("Settings clicked");
+        settingsMenu.SetActive(true);
+        buttonContianer.SetActive(false);
     }
     public void statsClicked()
     {
@@ -37,8 +67,33 @@ public class MainMenuScript : MonoBehaviour
     }
     public void quitClicked()
     {
+        buttonContianer.SetActive(false);
+        wantToQuitButton.SetActive(true);
+    }
+    public void wantToQuitClicked()
+    {
         print("Exiting Game...");
-        //Save the stats you want to save here
         Application.Quit();
+    }
+    public void inputSwitchClicked()
+    {
+        Text inputbuttonText = inputSwitchText.GetComponent<Text>();
+        if (inputbuttonText.text == "Input: Keyboard"){
+            inputbuttonText.text = "Input: Controller";
+        } else {
+            inputbuttonText.text = "Input: Keyboard";
+        }
+    }
+    public void volumeChanged()
+    {
+        Slider slider = volumeSlider.GetComponent<Slider>();
+        float sliderValue = slider.value;
+        print("Volume: " + Mathf.Round(sliderValue * 100f) + "%");
+    }
+    public void sensitivityChanged()
+    {
+        Slider slider = sensitivitySlider.GetComponent<Slider>();
+        float sliderValue = slider.value;
+        print("Sensitivity: " + Mathf.Round(sliderValue * 100f) + "%");
     }
 }
