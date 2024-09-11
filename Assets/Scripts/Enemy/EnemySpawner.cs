ece20be9a25fabab4a2433916c41431ce6abb879
ClareG2004 <cgrady@mail.bradley.edu>
2024-09-11 14:34:37 -0500

    Some stuff i was working on

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -12,28 +12,49 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] float timeTillEnemiesSpawn;
+    [Tooltip("Make array size the amount of spawn points you want")]
+    [SerializeField] EnemySpawnPointEntry[] enemySpawnPoints;
+
+    private bool hasSpawnedEnemies;
+
     [System.Serializable]
     private class EnemySpawnPointEntry
     {
         public GameObject EnemyType;
         public Transform EnemySpawnPoint;
     }
-    [Tooltip("Make array size the amount of spawn points you want")]
-    [SerializeField] EnemySpawnPointEntry[] enemySpawnPoints;
+
+    private void Start()
+    {
+        hasSpawnedEnemies = false;
+    }
 
     private void Update()
     {
-        InputEvents.Instance.EnemySpawnStarted.AddListener(SpawnEnemies);
+        //InputEvents.Instance.EnemySpawnStarted.AddListener(SpawnEnemies);
+        timeTillEnemiesSpawn -= Time.deltaTime;
+        
+        if(!hasSpawnedEnemies && timeTillEnemiesSpawn <= 0)
+        {
+            SpawnEnemies();
+        }
     }
 
     private void SpawnEnemies()
     {
-        if (!InputEvents.EnemySpawnPressed)
+        /*if (!InputEvents.EnemySpawnPressed)
             return;
         for(int i = 0; i < enemySpawnPoints.Length; ++i)
         {
             Instantiate(enemySpawnPoints[i].EnemyType, enemySpawnPoints[i].EnemySpawnPoint);
         }
         InputEvents.EnemySpawnPressed = false;
+        */
+        for(int i = 0; i < enemySpawnPoints.Length; i++)
+        {
+            Instantiate(enemySpawnPoints[i].EnemyType, enemySpawnPoints[i].EnemySpawnPoint);
+        }
+        hasSpawnedEnemies=true;
     }
 }
