6671a4cf5ef3d784e8a5f88051d5556f7e287cec
ClareG2004 <cgrady@mail.bradley.edu>
2024-09-20 20:13:26 -0500

    Line Stuff

---
+++ Assets/Scripts/Bullet Types/LineControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] LineRenderer lineRenderer;
+    private float waitTimeWeWant =5f;
+    private float currentWait;
+
+    private void Start()
+    {
+        currentWait = waitTimeWeWant;
+    }
+    private void Update()
+    {
+        if (gameObject != null)
+        {
+            Despawn();
+        }
+        currentWait -= Time.deltaTime;
+    }
+    public void Spawn(Vector3 origin, Vector3 destination, EnemyTakeDamage enemy)
+    {
+        Debug.Log("Made it to spawn");
+        LineRenderer lineRender = GetComponent<LineRenderer>();
+        lineRender.SetPosition(0, origin);
+        lineRender.SetPosition(1, destination);
+        lineRender.enabled = true;
+        Instantiate(lineRender, enemy.transform.position, Quaternion.identity);
+        
+    }
+
+    private void Despawn()
+    {
+        if(currentWait <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
