e674acffaa6155ce843b4a708f230bc59023064e
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2024-09-26 15:32:56 -0500

    possible explosion fix

--- Assets/Scripts/Bullet Types/Effects/ExplosionBullet.cs
+++ Assets/Scripts/Bullet Types/Effects/ExplosionBullet.cs
@@ -28,33 +28,23 @@ namespace DefaultNamespace
             RaycastHit[] hits = Physics.SphereCastAll(enemyOrgin, enemyHitExplotionRadius, 0.1f, enemyLayer);
             for (int i = 1; i < hits.Length; i++) 
             {
-                if (hits[i].collider.gameObject.TryGetComponent(out EnemyTakeDamage enemyDmg))
-                {
-                    enemyDmg.TakeDamage(damage * DamageMultiplier);
-                    Debug.Log(hits[i].collider.gameObject.name + " took " + (damage * DamageMultiplier) + " damage");
-                }
+                hits[i].collider.GetComponent<EnemyTakeDamage>().TakeDamage(damage * DamageMultiplier);
+                //Debug.Log(hits[i].collider.gameObject.name + " took " + (damage * DamageMultiplier) + " damage");
             }
-
             GameObject explosion = Instantiate(explosionPrefab, enemy.transform.position, Quaternion.identity);
-            explosion.transform.localScale = Vector3.one * enemyHitExplotionRadius;
+            explosion.transform.localScale = Vector3.one * enemyHitExplotionRadius * 2;
             explosion.GetComponent<DestroyObjectAfterSeconds>().Destroy(0.3f);
         }
-        //corutine is tempory and should be removed when we add an animation
-
         public override void OnHitOther(Vector3 point, float damage)
         {
             RaycastHit[] hits = Physics.SphereCastAll(point, surfaceHitExplosionRadius, Vector3.zero,0.1f, enemyLayer);
             for (int i = 1; i < hits.Length; i++)
             {
-                if(hits[i].collider.gameObject.TryGetComponent(out EnemyTakeDamage enemy))
-                {
-                    enemy.TakeDamage(damage * DamageMultiplier);
-                    Debug.Log(hits[i].collider.gameObject.name + " took " + (damage * DamageMultiplier) + " damage");
-                }
+                hits[i].collider.GetComponent<EnemyTakeDamage>().TakeDamage(damage * DamageMultiplier);
+                //Debug.Log(hits[i].collider.gameObject.name + " took " + (damage * DamageMultiplier) + " damage");
             }
-
             GameObject explosion = Instantiate(explosionPrefab, point, Quaternion.identity);
-            explosion.transform.localScale = Vector3.one * surfaceHitExplosionRadius;
+            explosion.transform.localScale = Vector3.one * surfaceHitExplosionRadius * 2;
             explosion.GetComponent<DestroyObjectAfterSeconds>().Destroy(0.3f);
         }
     }
