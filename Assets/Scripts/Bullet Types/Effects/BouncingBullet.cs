0191f332b5541bf39b9d5f67655fad2b3160969b
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2024-10-02 13:00:16 -0500

    test scene

--- Assets/Scripts/Bullet Types/Effects/BouncingBullet.cs
+++ Assets/Scripts/Bullet Types/Effects/BouncingBullet.cs
@@ -1,3 +1,10 @@
+/*******************************************************************************
+* File Name :         ExplosionBullet.cs
+* Author(s) :         Tyler B
+* Creation Date :     3/25/2024
+*
+* Brief Description : bounces off a wall twards the nearest enemy. basicly aim bot
+ *****************************************************************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +15,19 @@ namespace DefaultNamespace
     public class BouncingBullet : BulletEffect
     {
 
+        [SerializeField] private LayerMask enemyLayer;
+        [SerializeField] private float cosestEnemyDetectionRaduis = 30;
         public override void Initialize() { }
         public override void OnEnemyHit(EnemyTakeDamage type, float damage)
         {
-
+            // I dont think that this needs to do anything
         }
         public override void OnHitOther(Vector3 point, float damage)
         {
-
+            Collider[] enemyHits = Physics.OverlapSphere(point, cosestEnemyDetectionRaduis, enemyLayer);
+            GameObject explosion = Instantiate(explosionPrefab, point, Quaternion.identity);
+            explosion.transform.localScale = Vector3.one * surfaceHitExplosionRadius * 2;
+            explosion.GetComponent<DestroyObjectAfterSeconds>().Destroy(0.3f);
         }
     }
 }
