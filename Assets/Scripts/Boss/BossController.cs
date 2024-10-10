e3be6154db4a5aa2ed979336c057d18a1cac0b28
Sky Beal <thesnugpug1@gmail.com>
2024-10-09 21:38:09 -0500

    yeah

--- Assets/Scripts/Boss/BossController.cs
+++ Assets/Scripts/Boss/BossController.cs
@@ -8,24 +8,39 @@ using UnityEngine;
 
 public class BossController : Singleton<BossController>
 {
+
+    public float thwapDistance;
     [HideInInspector] public static BossStats Stats;
     [HideInInspector] public static Transform Player;
+    [HideInInspector] public static Rigidbody PlayerRB;
     [HideInInspector] public static PlayerBehaviour playerBehaviour;
     [HideInInspector] public static BossTakeDamage bossTakeDamage;
     [HideInInspector] public static bool Invincible = false;
     [HideInInspector] public static EnemySpawner enemySpawner;
     [HideInInspector] public static bool bossActive= false;
+    [HideInInspector] public static Transform Boss;
+    [HideInInspector] public static Animator animator;
 
     private void Start()
     {
         Stats = GetComponent<BossStats>();
         Player = GameObject.FindObjectOfType<PlayerController>().transform;
+        PlayerRB = Player.GetComponent<Rigidbody>();
+        Boss = GameObject.FindObjectOfType<BossController>().transform;
+        animator = Boss.GetComponent<Animator>();
         bossTakeDamage = GetComponent<BossTakeDamage>();
         playerBehaviour = GameObject.FindObjectOfType<PlayerBehaviour>();
         enemySpawner = GameObject.FindObjectOfType<EnemySpawner>();
         PublicEvents.OnPlayerDeath.AddListener(OnPlayerDeath);
     }
 
+    private void Update()
+    {
+        if (Vector3.Distance(Player.position, Boss.position) <= thwapDistance)
+        {
+            animator.SetBool("TailThwap", true);
+        }
+    }
 
     private void OnPlayerDeath()
     {
