664df751daa695acea1cb501f450f68c709694af
Sky Beal <thesnugpug1@gmail.com>
2024-10-14 13:32:55 -0500

    no more boss

--- Assets/Scripts/Boss/States/BossIdleBehaviour.cs
+++ Assets/Scripts/Boss/States/BossIdleBehaviour.cs
@@ -9,6 +9,7 @@ public class BossIdleBehaviour : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         nextState = Random.Range(1, 4);
+        BossController.Invincible = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
