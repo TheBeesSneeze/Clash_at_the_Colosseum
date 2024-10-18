68fec497b17b8511165f1d48634381800a59c708
Sky Beal <thesnugpug1@gmail.com>
2024-10-18 00:34:41 -0500

    wip

--- Assets/Scripts/ParticleManager.cs
+++ Assets/Scripts/ParticleManager.cs
@@ -4,35 +4,56 @@ using UnityEngine;
 //using UnityEngine.ParticleSystemModule;
 using UnityEngine.ParticleSystemJobs;
 
-public class ParticleManager : MonoBehaviour
+public class ParticleManager : Singleton<ParticleManager>
 {
+    public ParticleThing EnemySpawns;
     public void Start()
     {
-        
+        PublicEvents.OnStageTransitionFinish.AddListener(EnemySpawns.Play);
+        InitalizeParticles(EnemySpawns);
+        EnemySpawns.Play(); // it also needs to play at beiginning at game
+
     }
 
-    public IEnumerator PlayParticleGroups(ParticleThing particles)
+    public void InitalizeParticles(ParticleThing particles)
     {
         foreach (ParticleSystem particle in particles.particles)
         {
-            if (particle != null) 
-                particle.Play();
+            if (!particles.PlayOnStart)
+                particle.Stop();
+
         }
+    }
+
+    //public static IEnumerator PlayParticleGroups(ParticleThing particles)
+    //{
         
-        yield return new WaitForSeconds(particles.PlayDuration);
+        
+        /*yield return new WaitForSeconds(particles.PlayDuration);
 
         foreach (ParticleSystem particle in particles.particles)
         {
             if (particle == null) 
                 particle.Stop();
         }
-
-    }
+        */
+    //}
 }
 
 [System.Serializable]
 public class ParticleThing
 {
-    public float PlayDuration = 1;
+    //public float PlayDuration = 1;
     public List<ParticleSystem> particles;
+    public bool PlayOnStart = false;
+
+    public void Play()
+    {
+        //ParticleManager.Instance.StartCoroutine(ParticleManager.PlayParticleGroups(this));
+        foreach (ParticleSystem particle in particles)
+        {
+            if (particle != null)
+                particle.Play();
+        }
+    }
 }
