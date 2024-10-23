6a5d501a38983479bdb2fe9ba4ed2f002996a4eb
Donny <dtraywick@mail.bradley.edu>
2024-10-23 13:23:18 -0500

    NO MORE DEBUG

--- Assets/Scripts/DebugControls.cs
+++ Assets/Scripts/DebugControls.cs
@@ -11,18 +11,18 @@ public class DebugControls : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            UpgradeSelectUI ui = GameObject.FindObjectOfType<UpgradeSelectUI>();
-            ui.OpenMenu();
-        }
+   // void Update()
+    //{
+      //  if (Input.GetKeyDown(KeyCode.P))
+        //{
+          //  UpgradeSelectUI ui = GameObject.FindObjectOfType<UpgradeSelectUI>();
+           // ui.OpenMenu();
+        //}
 
-        if (Input.GetKeyDown(KeyCode.H))
-        {
-            HealthSystem hs = GameObject.FindObjectOfType<HealthSystem>();
-            hs.addCharge(99999);
-        }
-    }
+        //if (Input.GetKeyDown(KeyCode.H))
+        //{
+          //  HealthSystem hs = GameObject.FindObjectOfType<HealthSystem>();
+            //hs.addCharge(99999);
+        //}
+    //}
 }
