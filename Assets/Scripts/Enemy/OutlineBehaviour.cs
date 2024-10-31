4dffe42325a95b26d336aaa6c25256bf0059139e
Sky Beal <thesnugpug1@gmail.com>
2024-10-31 17:41:35 -0500

    take advantage of my toils

---
+++ Assets/Scripts/Enemy/OutlineBehaviour.cs
@@ -0,0 +1,32 @@
+using mainMenu;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class OutlineBehaviour : MonoBehaviour
+{
+    
+    [Tooltip ("The amount of enemies remaining to activate outliner.")][Min(1)]
+    public int EnemiesLeftToEnableOutline;
+    public void Start()
+    {
+        PublicEvents.OnAnyEnemyDeath += OnEnemyDeath;
+    }
+
+    public void OnEnemyDeath(EnemyStats stats)
+    {
+        if (EnemySpawner.ReturnEnemyCount() <= EnemiesLeftToEnableOutline)
+        {
+            Outline outline = gameObject.GetComponentInChildren<Outline>();
+
+            if (outline != null)
+            {
+                outline.enabled = true;
+            }
+        }
+    }
+
+    public void OnDestroy()
+    {
+        PublicEvents.OnAnyEnemyDeath -= OnEnemyDeath;
+    }
+}
