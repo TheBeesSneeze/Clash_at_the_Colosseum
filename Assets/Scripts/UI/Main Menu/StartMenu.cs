5e5e2383471ec328a79c0dbd991bfc6b4d50779b
ClareG2004 <cgrady@mail.bradley.edu>
2024-11-08 08:46:32 -0600

    Revert "Merge branch 'main' into DeathScreen"
    This reverts commit c962e419530f804e8037507d07b16124203e2d11, reversing
    changes made to ca15d5d6399e262116938cf4097a202967fcf030.

--- Assets/Scripts/UI/Main Menu/StartMenu.cs
+++ Assets/Scripts/UI/Main Menu/StartMenu.cs
@@ -22,30 +22,29 @@ namespace mainMenu
         private Button _settingsButton;
         private Button _creditsButton;
         private Button _quitButton;
-        private string _sceneToLoad;
         //private Button _confirmExitButton;
 
-        public StartMenu(MainMenuInitializer menu, Button playButton, Button tutorialButton, Button creditsMenu, Button quitButton, CanvasGroup Group, string SceneToLoad)
+        public StartMenu(MainMenuInitializer menu, Button playButton, Button tutorialButton, Button settingMenu, Button creditsMenu, Button quitButton, CanvasGroup Group)
         {
             _mainMenu = menu;
             _playButton = playButton;
             _tutorialButton = tutorialButton;
+            _settingsButton = settingMenu;
             _creditsButton = creditsMenu;
             _quitButton = quitButton;
             group = Group;
-            _sceneToLoad = SceneToLoad;
             //_confirmExitButton = confirmExitButton;
 
             _playButton.onClick.AddListener(playClicked);
             _tutorialButton.onClick.AddListener(tutorialClicked);
+            _settingsButton.onClick.AddListener(settingsClicked);
             _creditsButton.onClick.AddListener(creditsClicked);
             _quitButton.onClick.AddListener(quitClicked);
         }
 
         public void playClicked()
         {
-            //_mainMenu.animator.SetTrigger("Gun Select");
-            SceneManager.LoadScene(_sceneToLoad);
+            _mainMenu.animator.SetTrigger("Gun Select");
             Debug.Log("play clicked");
             //_mainMenu.Start_toGunSelect();
         }
