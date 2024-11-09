bd1f7a370e7df38b26204d549f356a27d894d519
ClareG2004 <cgrady@mail.bradley.edu>
2024-11-08 21:27:31 -0600

    Push

--- Assets/Scripts/UI/BulletLeftUI.cs
+++ Assets/Scripts/UI/BulletLeftUI.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class BulletLeftUI : MonoBehaviour
 {
-    [SerializeField] private TMPro.TMP_Text text;
     [SerializeField] private GunController gunController;
+    [SerializeField] private Slider slider;
 
     private bool canInfiniteFire;
     // Start is called before the first frame update
     void Start()
     {
-        
-
         if (gunController == null) 
             gunController = GameObject.FindObjectOfType<GunController>();
 
@@ -23,20 +22,18 @@ public class BulletLeftUI : MonoBehaviour
             PublicEvents.OnPlayerShoot.AddListener(BulletUI);
             PublicEvents.OnPlayerReload.AddListener(BulletUI);
         }
-
         BulletUI();
+        slider.maxValue = gunController.shootingMode.ClipSize;
+        slider.value = gunController.shootingMode.ClipSize; 
+    }
+
+    private void SetValue(int value)
+    {
+        slider.value = value;
     }
 
     private void BulletUI()
     {
-        if (text == null)
-            return;
-        if (canInfiniteFire)
-        {
-            text.text = "infinty / infinty".ToString();
-            return;
-        }
-
-        text.text = gunController.GetShotsLeft().ToString() + "/" + gunController.GetShotsTillCoolDown().ToString();
+        SetValue(gunController.GetShotsLeft());
     }
 }
