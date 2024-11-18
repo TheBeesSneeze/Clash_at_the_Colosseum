21ccb5503640a3af8b358c3ae637af88a239a906
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-11-18 15:20:24 -0600

    yay animations

--- Assets/Scripts/Bullet Types/BulletEffect.cs
+++ Assets/Scripts/Bullet Types/BulletEffect.cs
@@ -28,8 +28,8 @@ public abstract class BulletEffect : ScriptableObject
     [Header("UI")]
     public string UpgradeName;
     public string UpgradeDescription;
-    [ShowAssetPreview]
-    public Sprite UpgradeIcon;
+    //[ShowAssetPreview]
+    public Sprite[] UpgradeIcon;
     public Color bodyColor=Color.white;
     public Color secondaryColor=Color.white;
 
