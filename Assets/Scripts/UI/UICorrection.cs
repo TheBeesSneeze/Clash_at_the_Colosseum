7aad862720b3cb8e3698c8b0616c4b9dd577ed33
bee <104741505+TheBeesSneeze@users.noreply.github.com>
2024-11-20 14:09:05 -0600

    TIPS TIPS!

--- Assets/Scripts/UI/UICorrection.cs
+++ Assets/Scripts/UI/UICorrection.cs
@@ -12,6 +12,11 @@ public class UICorrection : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Correct();
+    }
+
+    public void Correct()
+    {
         LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
         RectTransform[] bruteForce = GetComponentsInChildren<RectTransform>();
         foreach (RectTransform b in bruteForce)
